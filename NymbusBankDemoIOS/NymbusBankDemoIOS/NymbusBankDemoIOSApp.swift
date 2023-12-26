3b34ffc6fa0e28daf951103f935d05acfa0df23a
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2023-12-25 20:50:42 -0600

    Main Nymbus
    Nymbus Bank Base App

---
+++ NymbusBankDemoIOS/NymbusBankDemoIOS/NymbusBankDemoIOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  NymbusBankDemoIOSApp.swift
+//  NymbusBankDemoIOS
+//
+//  Created by Fivel Rangel on 25/12/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NymbusBankDemoIOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
